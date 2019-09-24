8dfc1558f60f8c753d8774afdc30941daeb8ec76
akhil thankachen <akhilthankachen1234@gmail.com>
2019-09-24 18:08:59 +0530

    express app initialized

---
+++ back_end/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const database_config = require('./config/database');
+const mode_config = require('./config/env');
+
+// database connection
+
+// express app initiation
+const port = 3000;
+var app = express();
+
+// routes
+
+// middleware declaration
+app.use(cors()); // cors in use
+app.use(bodyParser.json()); // body parser
+
+// app loop
+app.get('/', (req,res) => {
+    res.send('working');
+});
+app.listen(port, () => {
+    if(mode_config.mode == "dev"){
+        console.log('server started at port ' + port);
+    }
+});
---
+++ back_end/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://localhost/271017/auto',
+    secret: 'secret'
+}
