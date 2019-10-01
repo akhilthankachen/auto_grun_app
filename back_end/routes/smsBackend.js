a43b03b68b87b00df510b7757348184fed20bafc
akhil thankachen <akhilthankachen1234@gmail.com>
2019-10-02 05:28:44 +0530

    sms receive update

--- back_end/routes/smsBackend.js
+++ back_end/routes/smsBackend.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.post('/receive', (req,res,next) => {
     var content = req.body.comments;
     content = JSON.parse(content);
-    console.log(content.akhil);
+    console.log(content);
     res.send();
 });
 
