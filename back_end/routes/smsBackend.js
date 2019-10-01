bc769fc99b6cb5d6dbbb898c3f1dd4df7a06e9de
akhil thankachen <akhilthankachen1234@gmail.com>
2019-10-01 17:22:31 +0530

    json parse test on sms 2

--- back_end/routes/smsBackend.js
+++ back_end/routes/smsBackend.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/receive', (req,res,next) => {
-    var content = req.nody.comments;
+    var content = req.body.comments;
     content = JSON.parse(content);
     console.log(content.akhil);
     res.send();
