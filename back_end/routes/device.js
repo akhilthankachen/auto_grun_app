e092a02fd01c8e0150ae77757f76e96f29d5f4ee
akhil thankachen <akhilthankachen1234@gmail.com>
2020-01-13 11:47:49 +0530

    lastTemp bug fix

--- back_end/routes/device.js
+++ back_end/routes/device.js
@@ -130,6 +130,7 @@ router.get('/lastTemp', passport.authenticate('jwt',{session:false}), (req,res,n
                 console.log(err);
                 res.json({success: false,msg : "Internal err"});
             } else {
+                console.log(doc.mac)
                 DeviceTemp.findOne({mac: doc.mac},{ sort: {'timeStamp' : -1}}, (err,post) => {
                     res.json(post)
                 })
